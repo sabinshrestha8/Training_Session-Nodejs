52ca98f881c3f68c933a6136485bf9a675e1605d
sabin <sabinshrestha8888@gmail.com>
2021-09-15 09:28:26 +0545

    Lesson-1 (Introduction to datatypes, array, object & variables)

---
+++ index.js
@@ -0,0 +1,63 @@
+// Object
+// Array
+// Number 
+// string
+// boolean
+// float
+// DateTime
+// null
+// undefined
+
+let a = 1; //Number
+let b = "Hello World"; //string
+let c = true; //boolean
+let d = 1.5; //float
+let e = null;
+// let e = new Date(); 
+let f = Date.now();
+
+// console.log(a);
+// a = "IIMS";
+// console.log(a);
+
+// console.log(typeof f);
+
+let person = {
+    name: 'Yajindra',
+    age: 21,
+    address: "kathmandu",
+    phone: 9821456982,
+    birthDate: new Date(),
+    location: {
+        lat: 27.7172453,
+        lng: 85.323905,
+    }
+};
+
+console.log("This person name is:", person.name);
+console.log("This person address is:", person.address);
+console.log("This person Birth date is:", person.birthDate);
+
+
+let exampleArray = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+let exampleArray2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(exampleArray);
+console.log(exampleArray2);
+
+
+let studentNames = ["yajindra", "gaurav", "ram", "hari"];
+console.log(studentNames[3]);
+
+
+const p = "Ra-One";
+p = "G-One"
+console.log(p);
+
+
+/*=====Today's Learnings=========*/
+Variable
+datatypes
+object
+Array
