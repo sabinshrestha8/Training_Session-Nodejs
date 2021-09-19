fbb14b5131e6d476da23f8d9d741207433942ba0
sabin <sabinshrestha8888@gmail.com>
2021-09-19 09:07:45 +0545

    Operators in Javascript

---
+++ operators.js
@@ -0,0 +1,34 @@
+// let a = "1";
+// let b = "2";
+
+// console.log(parseInt(a) + parseInt(b));
+
+// Add and Minus operators
+let a = 2;
+let b = 1;
+// a = a + a;
+// a -= a;
+a += b;  // {shorthand method for a = a + b}
+
+// console.log(a);
+
+//Not equal to operator in (if statement)
+// if (1 !== 1) {
+//     console.log("this statement is true");
+// }
+
+// if (1 <= 1) {
+//     console.log("this statement is true");
+// }
+
+
+// Assignment Operators in (if-else statement)
+if (1 === "1") {
+    console.log("this statement is true");
+} else {
+    console.log("this statement is false");
+}
+
+
+// ternary operator
+console.log(1 === 1 ? "this statement is true" : "this statement is false");
