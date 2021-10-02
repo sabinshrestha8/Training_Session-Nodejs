9a559fe9ee8abc87359382ef1c5fd9c7dfadfd94
sabin <sabinshrestha8888@gmail.com>
2021-10-02 12:29:42 +0545

    Promise chaining example added

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,65 +1,3 @@
-// const users = [{
-//     id: 1,
-//     name: "John",
-// },
-// {
-//     id: 2,
-//     name: "sara",
-// }]
-
-// const getUserById = async (id) => {
-//     // const newUserArray = users.filter((user, index) => {
-//     //     return user.id === id;
-//     // });
-//     // console.log(newUserArray);
-
-//     // let foundUser = null;
-//     // for(const user of users) {
-//     //     console.log(user)
-//     //     if(user.id === id) {
-//     //         foundUser = user;
-//     //     }
-//     // }
-
-//     // const updateName = (id, name) => {
-//     //     const user = getUserById(id);
-//     //     console.log("before change: ", user);
-
-//     //     if (user) {
-//     //         user.name = name;
-//     //         console.log("after change: ", user);
-//     //     }
-//     // }
-
-// }
-
-// // console.log(() => {
-// //     return "Hello world";
-// // })();
-
-
-// (() => {
-//     const user = getUserById(1);
-// })();
-
-
-
-
-/*================================================*/
-
-// const getRequestBody = () => {
-//     return new Promise((resolve, reject) => {
-//         request
-//     })
-// }
-
-// (async() => {
-//     const result = await getRequestBody();
-//     console.log(result);
-// })();
-
-
-
 /*================ Async await ===================*/
 
 const users = [{
@@ -76,41 +14,37 @@ const users = [{
 }];
 
 const getUserById = (id) => {
-    // // find user by filter method
-    // const newUserArray = users.filter((user, index) => {
-    //     // console.log(user);
-    //     return user.id === id;
-    // });
-
-    // console.log(newUserArray);
-
-    // // find user by for of loop
-    // let foundUser = null;
-    //     for (const user of users) {
-    //         // console.log(user);
-    //         if (user.id === id) {
-    //             foundUser = user;
-    //         }
-    //     }
-    //     console.log(foundUser);
-
-    // find user by find method
-    const result = users.find((user, index) => {
-        return user.id === id;
+    return new Promise((resolve, reject) => {
+        // find user by find method
+        const result = users.find((user, index) => {
+            return user.id === id;
+        });
+        // console.log(result);
+        resolve(result);
     });
-    // console.log(result);
-    return result;
 };
 
 
-const updateName = (id, name) => {
-    const user = getUserById(id);
-    console.log("Before change:", user);
-    if (user) {
+const updateName = (user, name) => {
+    // console.log("Before change:", user);
+    return new Promise((resolve, reject) => {
+        if (!user) {
+            reject("User cannot be empty");
+        }
         user.name = name;
-        console.log("After change:", user);
-    }
+        resolve(user);
+    });
 };
 
-updateName(1, 'Ram');
-// getUserById(7);
+
+getUserById(3)
+    .then((result) => {
+        console.log("original user value:", result);
+        return updateName(result, "Hari")
+    })
+    .then((result) => {
+        console.log("new updated result:", result);
+    })
+    .catch((err) => {
+        console.log("Error:", err);
+    });
